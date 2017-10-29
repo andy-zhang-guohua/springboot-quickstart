301bc1cedfd48d9788b23cd024e0382580c22899
ZhangGuohua <zhangguohua@yunintegration.com>
2017-10-29 16:32:15 +0800

    initial commit

---
+++ src/main/java/andy/tut/springboot/zero/Application.java
@@ -0,0 +1,14 @@
+package andy.tut.springboot.zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ZhangGuohua on 2017/10/29.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
