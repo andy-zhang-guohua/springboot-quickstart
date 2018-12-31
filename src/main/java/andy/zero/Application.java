0bbe220eb7507a74d40f98ebbcdd457ec90d2422
zhangguohua <zhangguohua@yunintegration.com>
2018-12-31 13:20:44 +0800

    more debug info

--- src/main/java/andy/zero/Application.java
+++ src/main/java/andy/zero/Application.java
@@ -1,16 +1,31 @@
 package andy.zero;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
 
 /**
  * Created by ZhangGuohua on 2017/10/29.
  */
+@Slf4j
 @SpringBootApplication
-public class Application {
-    public static void main(String[] args)   {
+public class Application implements CommandLineRunner {
+    @Autowired
+    ConfigurableWebApplicationContext applicationContext;
+
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("bean names:{}", applicationContext.getBeanDefinitionNames());
     }
 }
