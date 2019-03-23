b9e45107183314877baf740893c40f4fbf826ec0
andy.zhang.guohua <andy.zhang.guohua@gmail.com>
2019-03-23 23:14:59 +0800

    DI test/demo/debug

--- src/main/java/andy/tut/springboot/zero/controller/HomeController.java
+++ src/main/java/andy/tut/springboot/zero/controller/HomeController.java
@@ -2,11 +2,17 @@ package andy.tut.springboot.zero.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.ServletRequest;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by ZhangGuohua on 2017/10/29.
@@ -33,9 +39,11 @@ public class HomeController {
         this.methodRequest = request;
     }
 
-    @RequestMapping("/")
+    @RequestMapping(value="/",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public String home() {
-        return "Hello " + methodRequest.getParameter("name");
+    public Map home(@RequestParam int name) {
+        Map map=new TreeMap();
+        map.put("message","Hello " + name);
+         return map;
     }
 }
