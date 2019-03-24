f2044feda18c1eebd20b43cdb257ca85ff20d7a2
andy.zhang.guohua <andy.zhang.guohua@gmail.com>
2019-03-24 11:44:00 +0800

    HandlerMethodArgumentResolver

--- src/main/java/andy/tut/springboot/zero/controller/HomeController.java
+++ src/main/java/andy/tut/springboot/zero/controller/HomeController.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.*;
 
 import javax.servlet.ServletRequest;
 import java.util.Map;
@@ -22,6 +25,29 @@ public class HomeController {
 
     @Value("${city}")
     String city;
+
+    RequestParamMethodArgumentResolver requestParamMethodArgumentResolver;
+    RequestParamMapMethodArgumentResolver requestParamMapMethodArgumentResolver;
+    PathVariableMethodArgumentResolver pathVariableMethodArgumentResolver;
+    MatrixVariableMethodArgumentResolver matrixVariableMethodArgumentResolver;
+    ServletModelAttributeMethodProcessor servletModelAttributeMethodProcessor;
+    RequestResponseBodyMethodProcessor requestResponseBodyMethodProcessor;
+    RequestPartMethodArgumentResolver requestPartMethodArgumentResolver;
+    RequestHeaderMethodArgumentResolver requestHeaderMethodArgumentResolver;
+    ServletCookieValueMethodArgumentResolver servletCookieValueMethodArgumentResolver;
+    ExpressionValueMethodArgumentResolver expressionValueMethodArgumentResolver;
+    SessionAttributeMethodArgumentResolver sessionAttributeMethodArgumentResolver;
+    RequestAttributeMethodArgumentResolver requestAttributeMethodArgumentResolver;
+    ServletRequestMethodArgumentResolver servletRequestMethodArgumentResolver;
+    ServletResponseMethodArgumentResolver servletResponseMethodArgumentResolver;
+    HttpEntityMethodProcessor httpEntityMethodProcessor;
+    RedirectAttributesMethodArgumentResolver redirectAttributesMethodArgumentResolver;
+    ModelMethodProcessor modelMethodProcessor;
+    MapMethodProcessor mapMethodProcessor;
+    ErrorsMethodArgumentResolver errorsMethodArgumentResolver;
+    SessionStatusMethodArgumentResolver sessionStatusMethodArgumentResolver;
+    UriComponentsBuilderMethodArgumentResolver uriComponentsBuilderMethodArgumentResolver;
+    RequestMappingHandlerAdapter requestMappingHandlerAdapter;
 
     public HomeController(ServletRequest request) {
         constructorRequest = request;
@@ -45,5 +71,13 @@ public class HomeController {
         Map map=new TreeMap();
         map.put("message","Hello " + name);
          return map;
+    }
+
+    @RequestMapping(value="/{name}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public Map userHome(@PathVariable String name) {
+        Map map=new TreeMap();
+        map.put("message","Hello are you " + name + "?");
+        return map;
     }
 }
