1fa35b7e311be8e6a80e12c50b4a53111cdd8af3
andy.zhang.guohua <andy.zhang.guohua@gmail.com>
2019-08-19 16:10:27 +0800

    HandlerMethodReturnValueHandler实践

--- src/main/java/andy/tut/springboot/zero/controller/HomeController.java
+++ src/main/java/andy/tut/springboot/zero/controller/HomeController.java
@@ -26,6 +26,7 @@ public class HomeController {
 
     /**
      * 实验自动ServletRequest到控制器单例构造函数参数
+     *
      * @param request
      */
     public HomeController(ServletRequest request) {
@@ -48,6 +49,7 @@ public class HomeController {
 
     /**
      * 实验自动ServletRequest到控制器单例对象方法
+     *
      * @param request
      */
     @Autowired
@@ -55,38 +57,31 @@ public class HomeController {
         this.methodRequest = request;
     }
 
-    @RequestMapping(value="/",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public Map home() {
-        Map map=new TreeMap();
-        map.put("message","这是首页");
+        Map map = new TreeMap();
+        map.put("message", "这是首页");
         map.put("now", LocalDateTime.now());
-         return map;
+        return map;
     }
 
     /**
      * 观察特性: @ResponseStatus 的使用，效果 :
      * 1. /hello/abc ==> 400 错误，参数 name 类型不匹配
      * 2. /hello/1 ==> Whitelabel Error Page, 500 错误， 错误原因 : test
+     *
      * @param name
      * @return
      */
-    @ResponseStatus(reason="test")
-    @RequestMapping(value="/hello/{name}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseStatus(reason = "test")
+    @RequestMapping(value = "/hello/{name}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
     public Map helloName(@PathVariable int name) {
-        Map map=new TreeMap();
-        map.put("message","Hello, are you " + name + "?");
+        Map map = new TreeMap();
+        map.put("message", "Hello, are you " + name + "?");
         return map;
     }
 
-    /**
-     * 实验跳转到根路径，观察此时view name ==> View 的解析过程，以及所使用的 View 对象是什么：
-     * 应该是 RedirectView
-     * @return
-     */
-    @RequestMapping(value="/redirect-home")
-    public String redirectToHome() {
-       return "redirect:/";
-    }
+
 }
