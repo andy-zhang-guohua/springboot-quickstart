7f0f54b1dbda00a0f50c470764a0cac11b5850f0
ZhangGuohua <zhangguohua@kingsoft.com>
2021-06-30 20:44:36 +0800

    drools

---
+++ src/main/java/andy/zero/IrssetDroolsVo.java
@@ -0,0 +1,13 @@
+package andy.zero;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class IrssetDroolsVo {
+    private Integer surpDayCnt = null;
+    private boolean mBlack = false;
+    private String msg;
+
+}
