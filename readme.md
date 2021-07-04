d5dd7c3e90a3308a4d7a7bd369590e08cfc2f7f6
ZhangGuohua <786188617@qq.com>
2021-07-04 19:30:10 +0800

    基于antlr4的第一个语言，简单计算器

--- readme.md
+++ readme.md
@@ -1,19 +1,3 @@
-# 学习/演示基于 MyBatis 的一些知识点
-
-* 项目特点 :
-    * 单数据源
-    * MyBatis
-    * MySQL
-
-
-* 学习/演示知识点 :
-    * Entity 实体定义 : 完全 POJO, 不包含领域模型属性<-->表字段
-    * DAO Mapper 定义 ： 完全 接口， 指定领域模型属性<-->表字段映射,指定 原生 SQL 语句
-    * 组件识别 : 应用上注解 @MapperScan
-
 # 参考资料
-[spring boot 如何优雅的使用mybatis-spring-boot-starter](https://blog.csdn.net/zmx729618/article/details/80773887)
 
-
-
-
+- [Antlr4 IDEA 计算器入门小例](https://www.jianshu.com/p/628f2a4eb815)
--- src/main/java/andy/zero/Application.java
+++ src/main/java/andy/zero/Application.java
@@ -1,16 +1,14 @@
 package andy.zero;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by ZhangGuohua on 2017/10/29.
  */
-@MapperScan(basePackages = {"andy.zero.repo"})
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)   {
         SpringApplication.run(Application.class, args);
     }
 }
