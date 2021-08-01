10119a364f56015fb9d20bcd61d2db07f49c7403
ZhangGuohua <zhangguohua@kingsoft.com>
2021-08-01 18:15:55 +0800

    refactor

--- src/main/java/andy/docx4j/Docx4jDumpUtils.java
+++ src/main/java/andy/docx4j/Docx4jDumpUtils.java
@@ -10,8 +10,9 @@ import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.FooterPart;
 import org.docx4j.openpackaging.parts.WordprocessingML.HeaderPart;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
-import org.docx4j.wml.SectPr;
+import org.docx4j.wml.*;
 
+import javax.xml.bind.JAXBElement;
 import java.util.List;
 import java.util.StringJoiner;
 
@@ -25,40 +26,151 @@ public class Docx4jDumpUtils {
      * 3. 能够隐藏某些元素
      */
     public static void dumpElements(WordprocessingMLPackage wordMLPackage) {
-        try {
-            MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
-
-            List<Object> objects = documentPart.getContent();
-            log.info("主文档内容 : {} 个对象 => {}", objects.size(), objects);
-
-            DocumentModel documentModel = wordMLPackage.getDocumentModel();
-            List<SectionWrapper> sections = documentModel.getSections();
-            log.info("主文档包含 Section 数量 : {}", sections.size());
+        DocumentModel documentModel = wordMLPackage.getDocumentModel();
+        List<SectionWrapper> sections = documentModel.getSections();
+        log.info("主文档包含 Section 数量 : {}", sections.size());
 
 
-            for (int i = 0; i < sections.size(); i++) {
-                SectionWrapper section = sections.get(i);
+        for (int i = 0; i < sections.size(); i++) {
+            SectionWrapper section = sections.get(i);
 
-                String title = "Section [" + (i + 1) + "] ====================> ";
-                dumpSection(1, title, section);
-            }
+            String title = "Section [" + (i + 1) + "] ====================> ";
+            dumpSection(1, title, section);
+        }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
+
+        List<Object> objects = documentPart.getContent();
+        log.info("主文档内容 : {} 个对象 => {}", objects.size(), objects);
+        for (int i = 0; i < objects.size(); i++) {
+            String title = "元素[" + (i + 1) + "]";
+            dumpContentElement(1, title, objects.get(i));
         }
     }
+
+    private static void dumpContentElement(int level, String title, Object element) {
+        if (element instanceof P) {
+            dumpP(level, title, (P) element);
+            return;
+        }
+
+        if (element instanceof JAXBElement) {
+            dumpJAXBElement(level, title, (JAXBElement) element);
+            return;
+        }
+        String padding = prefixPadding(level);
+        log.info("{}{}", padding, title);
+    }
+
+    private static void dumpP(int level, String title, P element) {
+        String padding = prefixPadding(level);
+        log.info("{}P{} : {}", padding, title, element);
+    }
+
+    private static void dumpJAXBElement(int level, String title, JAXBElement element) {
+        String padding = prefixPadding(level);
+        Object value = element.getValue();
+        if (value == null) {
+            log.info("{}JAXBElement{} : {}", padding, title, element);
+            return;
+        }
+
+        if (value instanceof Tbl) {
+            dumpTbl(level, title, (Tbl) value);
+            return;
+        }
+    }
+
+    private static void dumpTbl(int level, String title, Tbl tbl) {
+        String padding = prefixPadding(level);
+        log.info("{}Tbl{} : {}", padding, title, tbl);
+
+        TblGrid tblGrid = tbl.getTblGrid();
+
+        String titleTblGrid = "表列信息";
+        dumpTblGrid(level + 1, titleTblGrid, tblGrid);
+
+        List<Object> list = tbl.getContent();
+        String titleTblContent = "表格内容";
+        dumpTblContent(level + 1, titleTblContent, list);
+    }
+
+    private static void dumpTblGrid(int level, String title, TblGrid tblGrid) {
+        String padding = prefixPadding(level);
+
+        String textTblGrid = toString(tblGrid);
+        log.info("{}{} : {}", padding, title, textTblGrid);
+    }
+
+    private static void dumpTblContent(int level, String title, List<Object> tblContent) {
+        String padding = prefixPadding(level);
+
+        if (tblContent.isEmpty()) {
+            log.info("{}{} : {}", padding, title, tblContent);
+            return;
+        }
+
+        log.info("{}{} : {}", padding, title);
+        for (int i = 0; i < tblContent.size(); i++) {
+            Object child = tblContent.get(i);
+            String childTitle = title + child.getClass().getName() + "[" + (level) + "]";
+            dumpTblContentElement(level + 1, childTitle, child);
+        }
+    }
+
+    private static void dumpTblContentElement(int level, String title, Object object) {
+        String padding = prefixPadding(level);
+
+        if (object instanceof Tr) {
+            dumpTr(level, title, (Tr) object);
+        } else {
+            log.info("{}{} : {}", padding, title, object);
+        }
+    }
+
+    private static void dumpTr(int level, String title, Tr tr) {
+        String padding = prefixPadding(level);
+        log.info("{}{} : {}", padding, title, tr);
+    }
+
+    private static String toString(TblGrid tblGrid) {
+        StringJoiner sj = new StringJoiner(" , ");
+        List<TblGridCol> tblGridCols = tblGrid.getGridCol();
+        for (int i = 0; i < tblGridCols.size(); i++) {
+            TblGridCol col = tblGridCols.get(i);
+            sj.add("列[" + (i + 1) + "]宽 : " + col.getW());
+        }
+
+        return sj.toString();
+    }
+
 
     private static void dumpSection(int level, String title, SectionWrapper section) {
         String padding = prefixPadding(level);
         log.info("{}{}", padding, title);
 
+        // 页面维度信息
         PageDimensions pageDimensions = section.getPageDimensions();
-        dumpPageSize(level + 1, "页面尺寸", pageDimensions.getPgSz());
-        dumpPageMargin(level + 1, "页面边距", pageDimensions.getPgMar());
 
+        dumpPageDimensions(level + 1, "页面维度", pageDimensions);
+
+        /// 页眉页脚策略
         HeaderFooterPolicy headerFooterPolicy = section.getHeaderFooterPolicy();
-        dumpHeaderFooterPolicy(level + 1, "", headerFooterPolicy);
+        dumpHeaderFooterPolicy(level + 1, "页眉页脚策略", headerFooterPolicy);
     }
+
+
+    private static void dumpPageDimensions(int level, String title, PageDimensions pageDimensions) {
+        String padding = prefixPadding(level);
+        log.info("{}{}", padding, title);
+
+        //// 页面尺寸
+        dumpPageSize(level + 1, "页面尺寸", pageDimensions.getPgSz());
+
+        //// 页面边距
+        dumpPageMargin(level + 1, "页面边距", pageDimensions.getPgMar());
+    }
+
 
     private static void dumpPageSize(int level, String title, SectPr.PgSz pageSize) {
         String padding = prefixPadding(level);
@@ -71,23 +183,26 @@ public class Docx4jDumpUtils {
     }
 
     private static void dumpHeaderFooterPolicy(int level, String title, HeaderFooterPolicy headerFooterPolicy) {
+        String padding = prefixPadding(level);
+        log.info("{}{}", padding, title);
+
         HeaderPart firstHeader = headerFooterPolicy.getFirstHeader(); // 首页页眉
-        dumpHeaderPart(level, "首页-页眉内容", firstHeader);
+        dumpHeaderPart(level + 1, "首页-页眉内容", firstHeader);
 
         FooterPart firstFooter = headerFooterPolicy.getFirstFooter(); // 首页页脚
-        dumpFooterPart(level, "首页-页脚内容", firstFooter);
+        dumpFooterPart(level + 1, "首页-页脚内容", firstFooter);
 
         HeaderPart defaultHeader = headerFooterPolicy.getDefaultHeader(); // 默认页面页眉
-        dumpHeaderPart(level, "默认-页眉内容", defaultHeader);
+        dumpHeaderPart(level + 1, "默认-页眉内容", defaultHeader);
 
         FooterPart defaultFooter = headerFooterPolicy.getDefaultFooter(); // 默认页面页脚
-        dumpFooterPart(level, "默认-页脚内容", defaultFooter);
+        dumpFooterPart(level + 1, "默认-页脚内容", defaultFooter);
 
         HeaderPart evenHeader = headerFooterPolicy.getEvenHeader(); // 偶数页面页眉
-        dumpHeaderPart(level, "偶数-页眉内容", evenHeader);
+        dumpHeaderPart(level + 1, "偶数-页眉内容", evenHeader);
 
         FooterPart evenFooter = headerFooterPolicy.getEvenFooter(); // 偶数页面页脚
-        dumpFooterPart(level, "偶数-页脚内容", evenFooter);
+        dumpFooterPart(level + 1, "偶数-页脚内容", evenFooter);
     }
 
     private static void dumpHeaderPart(int level, String title, HeaderPart headerPart) {
