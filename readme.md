bad9a217416c39c792016a7aeb5e7202ec730620
ZhangGuohua <zhangguohua@kingsoft.com>
2021-12-07 19:41:29 +0800

    Jacob 转 word 到 pdf

--- readme.md
+++ readme.md
@@ -1,4 +1,19 @@
 # 项目介绍
 
 - springboot 非 web 项目
-- 用于调查使用 poi 转 docx 到 pdf 文件
+- 用于调查使用 jacob 转 docx 到 pdf 文件
+- [下载 : jacob-1.20.zip](https://sourceforge.net/projects/jacob-project/files/)
+- 解压缩到 D:\programs\jacob-1.20
+- mvn install:install-file -Dfile=D:\programs\jacob-1.20\jacob.jar  -DgroupId=com.jacob -DartifactId=jacob  -Dversion=1.20 -Dpackaging=jar
+- pom 依赖
+```xml
+<!-- word转pdf（依赖windows本地的wps） -->
+        <dependency>
+            <groupId>com.jacob</groupId>
+            <artifactId>jacob</artifactId>
+            <version>1.20</version>
+        </dependency>
+```    
+- 在jdk/bin目录下引入.dll文件
+    - 64位：jacob-1.20-x64.dll       
+    - 32位：jacob-1.20-x86.dll
---
+++ src/main/java/com/andy/rhino/test/JacobWord2PdfTest.java
@@ -0,0 +1,30 @@
+package com.andy.rhino.test;
+
+
+import com.andy.rhino.utils.JacobPdfUtils;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JacobWord2PdfTest {
+
+
+    public static void testWord2PDF() {
+        String folder = "D:\\idea_wks\\springboot-quickstart\\src\\main\\resources\\";
+        String word = "TEST-WORD.docx";
+        String pdfByWPS = "TEST-jacob-wps.pdf";
+        String pdfByWORD = "TEST-jacob-word.pdf";
+
+        String wordFile = folder + word;
+        String pdfFileFromWPS = folder + pdfByWPS;
+        String pdfFileFromWORD = folder + pdfByWORD;
+
+        JacobPdfUtils.convertWordToPdfUsingWPS(wordFile, pdfFileFromWPS);
+        JacobPdfUtils.convertWordToPdfUsingMSWord(wordFile, pdfFileFromWORD);
+    }
+
+
+    public static void main(String[] args) {
+        testWord2PDF();
+
+    }
+}
