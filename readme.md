43e2e4859d0b2a464ceae8640189f764acd91f87
ZhangGuohua <zhangguohua@kingsoft.com>
2021-12-13 15:45:20 +0800

    JUL

--- readme.md
+++ readme.md
@@ -1,25 +1,7 @@
 # 项目介绍
 
 - springboot 非 web 项目
-- 用于调查使用 jacob 转 docx 到 pdf 文件
-- [下载 : jacob-1.20.zip](https://sourceforge.net/projects/jacob-project/files/)
-- 解压缩到 D:\programs\jacob-1.20
-- mvn install:install-file -Dfile=D:\programs\jacob-1.20\jacob.jar  -DgroupId=com.jacob -DartifactId=jacob  -Dversion=1.20 -Dpackaging=jar
-- pom 依赖
-```xml
-<!-- word转pdf（依赖windows本地的wps） -->
-        <dependency>
-            <groupId>com.jacob</groupId>
-            <artifactId>jacob</artifactId>
-            <version>1.20</version>
-        </dependency>
-```    
-- 在jdk/bin目录下引入.dll文件
-    - 64位：jacob-1.20-x64.dll       
-    - 32位：jacob-1.20-x86.dll
+- 用于调查使用 JUL 的各种使用
     
 - 参考资料
-    - [java 将word转为PDF (100%与word软件转换一样)](https://www.cnblogs.com/mh-study/p/10342246.html) 
-    - [java实现word,ppt,excel,jpg转pdf](https://blog.csdn.net/xuchaozheng/article/details/19199721)
-    - [通过Jacob调用WPS将office文件转为PDF文件](https://blog.csdn.net/cocoaxian/article/details/81333273)
-    - [jacob操作word excel](https://www.iteye.com/topic/588050)
+    - [java线程池(二)：聊聊newFixedThreadPool(1)和newSingleThreadExecutor()的区别](https://www.jianshu.com/p/c94234ece339) 
---
+++ src/main/java/com/andy/jacob/test/OrderTest.java
@@ -0,0 +1,45 @@
+package com.andy.jacob.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+/**
+ * * - java线程池(二)：聊聊newFixedThreadPool(1)和newSingleThreadExecutor()的区别
+ * * -- https://www.jianshu.com/p/c94234ece339
+ * * -- 比较 Executors.newFixedThreadPool(1) 和 Executors.newSingleThreadExecutor() 在任务处理顺序上的异同
+ * * -- 结论 : 相同
+ */
+@Slf4j
+public class OrderTest {
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService1 = Executors.newFixedThreadPool(1);
+        ExecutorService executorService2 = Executors.newSingleThreadExecutor();
+
+        log.info("基于 Executors.newFixedThreadPool(1)");
+        testOrder(executorService1);
+
+        TimeUnit.SECONDS.sleep(1);
+
+        log.info("基于 Executors.newSingleThreadExecutor()");
+        testOrder(executorService2);
+
+    }
+
+    private static void testOrder(ExecutorService executorService) throws InterruptedException {
+        List<Integer> submit = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        IntStream.range(0, 10).forEach((i) -> {
+            submit.add(i);
+            executorService.submit(() -> result.add(i));
+        });
+        TimeUnit.SECONDS.sleep(1);
+        log.info("等待处理任务顺序 : {}", submit);
+        log.info("实际任务处理顺序 : {}", result);
+    }
+}
